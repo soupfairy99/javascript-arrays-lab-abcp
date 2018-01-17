de1d9c4a60ac2706ce8e6c266374d418fa09fce6
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-17 20:44:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+function
+destructivelyAppendKitten()
+{return kittens.push("Ralph")}
+function
+destructivelyPrependKitten()
+{return kittens.unshift("Bob")}
+function
+destructivelyRemoveLastKitten()
+{return kittens.pop("Garfield")}
+function
+destructivelyRemoveFirstKitten()
+{return kittens.shift('Milo')}
+function
+appendKitten()
+{return [...kittens,"Broom"]}
+function
+prependKitten()
+{return ["Arnold",...kittens]}
+function
+removeLastKitten()
+{var removeLastKitten = [kittens.slice(-1)];return removeLastKitten,kittens}
+/*function
+removeFirstKitten()
+*/
+var removeFirstKitten = [kittens.slice(-2)]
+console.log(removeFirstKitten);
+
+
+
+
+/*
+
+  
+
+  describe('removeFirstKitten()', function() {
+    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+})
+*/
